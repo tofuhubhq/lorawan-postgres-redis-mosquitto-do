c04f23a3bdc644fdfb0e264d9b2211a9832493e5
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-06-15 11:11:06 +0200

    bumped version

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -32,7 +32,7 @@ provider "digitalocean" {
   token = var.do_access_token
 }
 
-#@tofuhub:connects_to->chirpstack_nodes
+#!@tofuhub:connects_to->chirpstack_nodes
 resource "digitalocean_loadbalancer" "chirpstack_lb" {
   name   = "chirpstack-lb"
   region = var.do_chirpstack_droplet_region
