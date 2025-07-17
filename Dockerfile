0703e4492229b9dceb0d2d3d411e9cc3e1137cd0
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-07-17 16:46:28 +0200

    bugfix

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update && apt-get install -y \
     unzip \
     gnupg \
     git \
+    dnsutils \
     postgresql-client \
     ca-certificates \
     && rm -rf /var/lib/apt/lists/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,6 @@ vars=(
   do_chirpstack_droplet_image
   do_chirpstack_droplet_region
   do_chirpstack_firewall_name
-  do_ssh_key_name
   redis_droplet_name
   redis_droplet_size
   redis_droplet_image
